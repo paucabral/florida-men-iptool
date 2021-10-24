53df03318c78df6024e01b1670eeb8de3fc676e2
paucabral <josepaucabral@gmail.com>
2021-10-24 18:37:00 +0800

    Added boilerplate code for form submission using POST

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ app = Flask(__name__, template_folder='.')
 def home():
     ip_address = "192.168.254.1"
     ip_version = "ipv4"
+    ip_region = "Manila"
     ip_postal_code = "1800"
     ip_country = "Philippines"
     ip_timezone = "Asia/Manila"
@@ -20,6 +21,7 @@ def home():
     return render_template('index.html',
                            ip_address=ip_address,
                            ip_version=ip_version,
+                           ip_region=ip_region,
                            ip_postal_code=ip_postal_code,
                            ip_country=ip_country,
                            ip_timezone=ip_timezone,
@@ -32,7 +34,31 @@ def home():
 
 @app.route("/", methods=['POST'])
 def searchIP():
-    pass
+    ip_address = request.form.get('ip_address')
+
+    # values to return from API calls
+    ip_version = "ipv4"
+    ip_region = "Manila"
+    ip_postal_code = "1800"
+    ip_country = "Philippines"
+    ip_timezone = "Asia/Manila"
+    ip_calling_code = "+63"
+    ip_asn = "AS132199"
+    ip_isp = "PLDT Inc."
+    ip_lat = 14.674
+    ip_lng = 121.014
+    return render_template('index.html',
+                           ip_address=ip_address,
+                           ip_version=ip_version,
+                           ip_region=ip_region,
+                           ip_postal_code=ip_postal_code,
+                           ip_country=ip_country,
+                           ip_timezone=ip_timezone,
+                           ip_calling_code=ip_calling_code,
+                           ip_asn=ip_asn,
+                           ip_isp=ip_isp,
+                           ip_lat=ip_lat,
+                           ip_lng=ip_lng)
 
 
 if __name__ == '__main__':
