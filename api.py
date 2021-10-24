6a9134bbeb56a5e05adda84a28990770a2231546
JoshuaLopez0378 <joshuaalbertlopez0378@gmail.com>
2021-10-24 09:50:19 +0800

    Added api.py with primary function get

--- api.py
+++ api.py
@@ -10,12 +10,7 @@ def getSpecificIP(IPAdd):
 
     urlGet = get("http://ipapi.co/{}/json/".format(IPAdd))
     dicUrl = urlGet.json()
-    if 'error' not in dicUrl:      
-        filterDic = {item: dicUrl[item] for item in usedFields}
-        return filterDic
-    else:
-        errorDic = {item: dicUrl[item] for item in errorFields}
-        return errorDic
+    return dicUrl
 
 def getOwnIP():
     urlGet = get("https://ipapi.co/json")
