c778e7e661f31f587d5fcf539a4c1df0ea5b1987
Janrey Licas <self.codexlink@gmail.com>
2021-10-26 12:28:52 +0800

    Update (app.py): Reduce Complications for Checking Arguments.

--- app.py
+++ app.py
@@ -1,32 +1,22 @@
 from flask import Flask, redirect, url_for, render_template, request
 from api import errorFields, getSpecificIP, getOwnIP, rateLimitedFields
-from typing import Any, Dict, Optional, Union
+from typing import Any, Dict, Union
 import os
 
 app = Flask(__name__, template_folder='.')
 
 # ! This function handles both home() and searchIP() into one function.
-def ip_query(given_ip: str, meth: Union["GET", "POST"]) -> Dict[str, Any]:
-    # Pre-check on given arguments before processing.
-
-    # Asserts
-    if meth == "GET":
-        assert given_ip is None, "The given method is 'GET' but the callback contains 'given_ip'. This is unintended."
-
-    elif meth == "POST":
-        assert given_ip is not None, "The given method is 'POST', but the callback does not contain 'given_ip' This is unintended."
-        assert type(given_ip) is str, "The given IP is not a string! This is an error."
+def ip_query(given_ip: Union[None, str]) -> Dict[str, Any]:
     
-    else:
-        raise ValueError(f"The given method ({meth.upper()}) is not available! Please get to the basic API methods.")
+    assert type(given_ip) is str or given_ip is None, "The given IP is not a string! This is an error."
 
-    req_ip_data = getOwnIP() if meth == "GET" else getSpecificIP(given_ip) # Assumes POST based on checks.
+    req_ip_data = getOwnIP() if not given_ip else getSpecificIP(given_ip) # Assumes that given_ip has length.
 
-    # If all assertion test is passed, create a blueprint (dictionary).
     ip_info = {}
 
-    if req_ip_data: # Checks if has content.
-        if list(req_ip_data.keys()) != errorFields and list(req_ip_data.keys()) != rateLimitedFields: # Ensure that the given response doesn't contain fields declared under `errorFields` and `rateLimitedFields`.
+    if req_ip_data: # Check if the variable has a content.
+        if list(req_ip_data.keys()) != errorFields and list(req_ip_data.keys()) != rateLimitedFields:
+            # Ensure that the given response doesn't contain fields declared under `errorFields` and `rateLimitedFields`.
             ip_info = {
                 "address": req_ip_data["ip"],
                 "version": req_ip_data["version"],
@@ -42,7 +32,7 @@ def ip_query(given_ip: str, meth: Union["GET", "POST"]) -> Dict[str, Any]:
             }
         
 
-    else: # Evaluates to None. Map other errors for rate-limited and actual error given data.
+    else: # Evaluates to None. Map other errors for rate-limited and actual error given data. 
         ip_info = {
             "error": req_ip_data["ip"] if list(req_ip_data.keys()) == errorFields else req_ip_data["reason"],
             "reason": req_ip_data["reason"] if list(req_ip_data.keys()) != errorFields else req_id_data["message"]
@@ -52,16 +42,16 @@ def ip_query(given_ip: str, meth: Union["GET", "POST"]) -> Dict[str, Any]:
 
 @app.route("/", methods=['GET'])
 def home():
-    return render_template('index.html', ip_context = ip_query(None, "GET"))
+    return render_template('index.html', ip_context = ip_query(None))
 
 
 @app.route("/", methods=['POST'])
 def searchIP():
     # For test purposes, use "request.json.get()" instead. Error is not handled here.
-    ip_address = request.json.get("ip_address")
     # ip_address = request.form.get('ip_address') # Before
+    ip_address = request.json.get("ip_address") # After
 
-    return render_template('index.html', ip_context = ip_query(ip_address, "POST"))
+    return render_template('index.html', ip_context = ip_query(ip_address))
 
 
 if __name__ == '__main__':
