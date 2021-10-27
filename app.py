e84d95cbd5ba67aa6c5f0a28895f677d918f4609
paucabral <josepaucabral@gmail.com>
2021-10-27 20:35:48 +0800

    Error fixes after code review. Handled error boolean and reason to display in frontend accordingly.

--- app.py
+++ app.py
@@ -6,10 +6,14 @@ import os
 app = Flask(__name__, template_folder='.')
 
 # ! This function handles both home() and searchIP() into one function.
-def ip_query(given_ip: Union[None, str]) -> Dict[str, Any]:
-    assert type(given_ip) is str or given_ip is None, "The given IP is not a string! This is an error."
 
-    req_ip_data = getOwnIP() if not given_ip else getSpecificIP(given_ip) # Assumes that given_ip has length.
+
+def ip_query(given_ip: Union[None, str]) -> Dict[str, Any]:
+    assert type(
+        given_ip) is str or given_ip is None, "The given IP is not a string! This is an error."
+
+    # Assumes that given_ip has length.
+    req_ip_data = getOwnIP() if not given_ip else getSpecificIP(given_ip)
 
     # Ensure that the given response doesn't contain fields declared under `errorFields` and `rateLimitedFields`.
     return {
@@ -28,48 +32,54 @@ def ip_query(given_ip: Union[None, str]) -> Dict[str, Any]:
         "reason": req_ip_data.get("reason") if list(req_ip_data.keys()) == errorFields else req_ip_data.get("message")
     }
 
+
 @app.route("/", methods=['GET'])
 def home():
     fetched_data = ip_query(None)
-    return render_template('index.html',
-                           ip_address = fetched_data["address"],
-                           ip_version = fetched_data["version"],
-                           ip_region = fetched_data["region"],
-                           ip_postal_code = fetched_data["postal_code"],
-                           ip_country = fetched_data["country"],
-                           ip_timezone = fetched_data["tz"],
-                           ip_calling_code = fetched_data["calling_code"],
-                           ip_asn = fetched_data["asn"],
-                           ip_isp = fetched_data["isp"],
-                           ip_lat = fetched_data["lat"],
-                           ip_lng = fetched_data["lng"],
-                           error = fetched_data["error"],
-                           reason = fetched_data["reason"]
-        )
+    data = getOwnIP()
+    if "error" in data.keys() == True:
+        return render_template('index.html', error=True, reason=data["reason"])
+    else:
+        return render_template('index.html',
+                               ip_address=fetched_data["address"],
+                               ip_version=fetched_data["version"],
+                               ip_region=fetched_data["region"],
+                               ip_postal_code=fetched_data["postal_code"],
+                               ip_country=fetched_data["country"],
+                               ip_timezone=fetched_data["tz"],
+                               ip_calling_code=fetched_data["calling_code"],
+                               ip_asn=fetched_data["asn"],
+                               ip_isp=fetched_data["isp"],
+                               ip_lat=fetched_data["lat"],
+                               ip_lng=fetched_data["lng"],
+                               )
+
 
 @app.route("/", methods=['POST'])
 def searchIP():
     # For test purposes, use "request.json.get()" instead. Error is not handled here.
 
-    # ip_address = request.form.get('ip_address') # Before
-    ip_address = request.json.get("ip_address") # After
+    ip_address = request.form.get('ip_address')  # Before
+    # ip_address = request.json.get("ip_address") # After
     fetched_data = ip_query(ip_address)
+    data = getSpecificIP(ip_address)
+    if "error" in data.keys():
+        return render_template('index.html', error=True, reason=data["reason"])
+    else:
+        return render_template('index.html',
+                               ip_address=fetched_data["address"],
+                               ip_version=fetched_data["version"],
+                               ip_region=fetched_data["region"],
+                               ip_postal_code=fetched_data["postal_code"],
+                               ip_country=fetched_data["country"],
+                               ip_timezone=fetched_data["tz"],
+                               ip_calling_code=fetched_data["calling_code"],
+                               ip_asn=fetched_data["asn"],
+                               ip_isp=fetched_data["isp"],
+                               ip_lat=fetched_data["lat"],
+                               ip_lng=fetched_data["lng"],
+                               )
 
-    return render_template('index.html',
-                           ip_address = fetched_data["address"],
-                           ip_version = fetched_data["version"],
-                           ip_region = fetched_data["region"],
-                           ip_postal_code = fetched_data["postal_code"],
-                           ip_country = fetched_data["country"],
-                           ip_timezone = fetched_data["timezone"],
-                           ip_calling_code = fetched_data["calling_code"],
-                           ip_asn = fetched_data["asn"],
-                           ip_isp = fetched_data["isp"],
-                           ip_lat = fetched_data["lat"],
-                           ip_lng = fetched_data["lng"],
-                           error = fetched_data["error"],
-                           reason = fetched_data["reason"]
-        )
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
