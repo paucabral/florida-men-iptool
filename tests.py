34417666f4978f60fb857e8773b52a48e0d53137
Janrey Licas <self.codexlink@gmail.com>
2021-10-28 08:05:37 +0800

    Tests | Added 404 Assert Error Test

--- tests.py
+++ tests.py
@@ -11,25 +11,33 @@ RestContext = str
 INVALID_SPECIFIC_IP_ADDRESS: Final[IpAddress] = "142askdopewqipo" # This is just a random.
 VALID_SPECIFIC_IP_ADDRESS: Final[IpAddress] = "142.250.204.110" # Just a google test ping IP.
 HTTP_OKAY: Final[int] = 200
+HTTP_NOT_OKAY: Final[int] = 404
+RANDOM_URL: Final[str] = "pisadpjasdopasdkosadkoaswqepsxi"
 
 class IPTests(TestCase):
     def setUp(self):
         self.instance = app.test_client()
-
+    
     def test_get_own_ip(self):
         timeout(1)
         resp_own_ip = self.instance.get("/")
         self.assertTrue(HTTP_OKAY, resp_own_ip.status_code)
-
+     
     def test_assert_specific_ip(self):
-        timeout(2)
+        timeout(1.5)
         resp_req_specific_ip = self.instance.get("/", data={"ip_address": VALID_SPECIFIC_IP_ADDRESS})
         self.assertTrue(HTTP_OKAY, resp_req_specific_ip.status_code)
-
+  
     def test_get_okay_invalid_ip(self):
-        timeout(3)
+        timeout(1.5)
         resp_invalid_ip  = self.instance.get("/", data={"ip_address": INVALID_SPECIFIC_IP_ADDRESS})
         self.assertTrue(HTTP_OKAY, resp_invalid_ip.status_code)
+     
+   
+    def test_assert_invalid_url(self):
+        resp_invalid_url = self.instance.get(f"/{RANDOM_URL}") # Statically randomized.
+        self.assertEqual(HTTP_NOT_OKAY, resp_invalid_url.status_code)
+ 
 
 if __name__ == "__main__":
     entrypoint()
