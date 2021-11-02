87f8dda535296697965d8fae691ae3382ebbd672
paucabral <josepaucabral@gmail.com>
2021-11-02 13:58:03 +0800

    Resolved conflicts

--- api.py
+++ api.py
@@ -1,10 +1,43 @@
-import json
 from requests import get
+import json
+
+#Lists of keys to be used as the json is returned, to return a dictionary
+usedFields = ['ip', 'version', 'country', 'region', 'postal', 'country',
+              'timezone', 'country_calling_code', 'asn', 'org', 'latitude', 'longitude']    #Keys for success call
+errorFields = ['ip', "error", 'reason']                                                     #Keys for IP error (Invalid IP/ Reserved IP)
+rateLimitedFields = ['error', 'reason', 'message']                                          #Keys when API returns rate limited
 
 
-def getSpecificIP(ipaddress):
-    pass
+#Function for API call (specific IP input)
+def getSpecificIP(IPAdd):
+    urlGet = get("http://ipapi.co/{}/json/".format(IPAdd))
+    dicUrl = urlGet.json()
+
+    if 'error' not in dicUrl:   # If no errors are found, return dictionary with elements from usedFields as keys
+        filterDic = {item: dicUrl[item] for item in usedFields}
+        return filterDic
+
+    elif "error" in dicUrl:  # If rate-limited, just return that.
+        return dicUrl
+
+    else:                   # If errors found, return dictionary with elements from errorFields as keys
+        errorDic = {item: dicUrl[item] for item in errorFields}
+        return errorDic
 
 
-def getIP():
-    pass
+#Function for API call (autodetect host IP)
+def getOwnIP():
+    urlGet = get("https://ipapi.co/json")
+    dicUrl = urlGet.json()
+
+    # This also requires handling on rateLimited as well.
+    if "error" not in dicUrl:
+        filterDic = {item: dicUrl[item] for item in usedFields}
+        return filterDic
+
+    elif "error" in dicUrl:  # If rate-limited, just return that.
+        return dicUrl
+
+    else:                   # If errors found, also return elements from errorFields as keys
+        errorDic = {item: dicUrl[item] for item in errorFields}
+        return errorDic
--- app.py
+++ app.py
@@ -1,39 +1,87 @@
 from flask import Flask, redirect, url_for, render_template, request
-from api import getSpecificIP, getIP
-import os
+from api import errorFields, getSpecificIP, getOwnIP, rateLimitedFields
+from typing import Any, Dict, Union # For type-annotations.
 
 app = Flask(__name__, template_folder='.')
+
+# ! This function handles both home() and searchIP() into one function.
+def ip_query(given_ip: Union[None, str]) -> Dict[str, Any]:
+
+    # Assert that the supplied argument is type-restricted.
+    assert type(given_ip) is str or given_ip is None, "The given IP is not a string! This is an error."
+
+    # This block runs a function based on the supplied argument's length. If there is run getSpecificIP,
+    # otherwise run getOwnIP()
+    req_ip_data = getOwnIP() if not given_ip else getSpecificIP(given_ip)
+
+    # Inlined Approach |Eensure that the given response doesn't contain fields. Otherwise, return "error" and "reason".
+    return {
+        "address": req_ip_data.get("ip", None),
+        "version": req_ip_data.get("version", None),
+        "region": req_ip_data.get("region", None),
+        "postal_code": req_ip_data.get("postal", None),
+        "country": req_ip_data.get("country", None),
+        "timezone": req_ip_data.get("timezone", None),
+        "calling_code": req_ip_data.get("country_calling_code", None),
+        "asn": req_ip_data.get("asn", None),
+        "isp": req_ip_data.get("org", None),
+        "lat": req_ip_data.get("latitude", None),
+        "lng": req_ip_data.get("longitude", None),
+
+    } if "error" not in req_ip_data.keys() else {
+        "error": req_ip_data.get("error", None),
+        "reason": req_ip_data.get("reason", None)
+    }
 
 
 @app.route("/", methods=['GET'])
 def home():
-    ip_address = "192.168.254.1"
-    ip_version = "ipv4"
-    ip_postal_code = "1800"
-    ip_country = "Philippines"
-    ip_timezone = "Asia/Manila"
-    ip_calling_code = "+63"
-    ip_asn = "AS132199"
-    ip_isp = "PLDT Inc."
-    ip_lat = 14.674
-    ip_lng = 121.014
-    return render_template('index.html',
-                           ip_address=ip_address,
-                           ip_version=ip_version,
-                           ip_postal_code=ip_postal_code,
-                           ip_country=ip_country,
-                           ip_timezone=ip_timezone,
-                           ip_calling_code=ip_calling_code,
-                           ip_asn=ip_asn,
-                           ip_isp=ip_isp,
-                           ip_lat=ip_lat,
-                           ip_lng=ip_lng)
+    fetched_data = ip_query(None)
+
+    # If there's an "error" field, return only those error fields. Otherwise, return IP context.
+    if "error" in fetched_data.keys():
+        return render_template('index.html', error=fetched_data["error"], reason=fetched_data["reason"])
+
+    else:
+        return render_template('index.html',
+                               ip_address=fetched_data["address"],
+                               ip_version=fetched_data["version"],
+                               ip_region=fetched_data["region"],
+                               ip_postal_code=fetched_data["postal_code"],
+                               ip_country=fetched_data["country"],
+                               ip_timezone=fetched_data["timezone"],
+                               ip_calling_code=fetched_data["calling_code"],
+                               ip_asn=fetched_data["asn"],
+                               ip_isp=fetched_data["isp"],
+                               ip_lat=fetched_data["lat"],
+                               ip_lng=fetched_data["lng"],
+                               )
 
 
 @app.route("/", methods=['POST'])
 def searchIP():
-    pass
+    ip_address = request.form.get('ip_address') # Fetch the received form-data.
+    fetched_data = ip_query(ip_address) # And then query the given data to the API.
 
+    # If there's an "error" field, return only those error fields. Otherwise, return IP context.
+    if "error" in fetched_data.keys():
+        return render_template('index.html', error=fetched_data["error"], reason=fetched_data["reason"])
 
+    else:
+        return render_template('index.html',
+                               ip_address=fetched_data["address"],
+                               ip_version=fetched_data["version"],
+                               ip_region=fetched_data["region"],
+                               ip_postal_code=fetched_data["postal_code"],
+                               ip_country=fetched_data["country"],
+                               ip_timezone=fetched_data["timezone"],
+                               ip_calling_code=fetched_data["calling_code"],
+                               ip_asn=fetched_data["asn"],
+                               ip_isp=fetched_data["isp"],
+                               ip_lat=fetched_data["lat"],
+                               ip_lng=fetched_data["lng"],
+                               )
+
+# Driver Code
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
