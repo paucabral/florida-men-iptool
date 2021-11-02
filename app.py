cdb5a8c08c37a3a8d43803e8a4fa3e9684f016df
Janrey Licas <self.codexlink@gmail.com>
2021-11-02 09:34:02 +0800

    Update (annotations): Added.

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 from flask import Flask, redirect, url_for, render_template, request
 from api import errorFields, getSpecificIP, getOwnIP, rateLimitedFields
-from typing import Any, Dict, Union
-import os
+from typing import Any, Dict, Union # For type-annotations.
 
 app = Flask(__name__, template_folder='.')
 
 # ! This function handles both home() and searchIP() into one function.
 def ip_query(given_ip: Union[None, str]) -> Dict[str, Any]:
+
+    # Assert that the supplied argument is type-restricted.
     assert type(given_ip) is str or given_ip is None, "The given IP is not a string! This is an error."
 
-    # Assumes that given_ip has length.
+    # This block runs a function based on the supplied argument's length. If there is run getSpecificIP,
+    # otherwise run getOwnIP()
     req_ip_data = getOwnIP() if not given_ip else getSpecificIP(given_ip)
 
-    # Ensure that the given response doesn't contain fields declared under `errorFields` and `rateLimitedFields`.
+    # Inlined Approach |Eensure that the given response doesn't contain fields. Otherwise, return "error" and "reason".
     return {
         "address": req_ip_data.get("ip", None),
         "version": req_ip_data.get("version", None),
@@ -36,6 +38,7 @@ def ip_query(given_ip: Union[None, str]) -> Dict[str, Any]:
 def home():
     fetched_data = ip_query(None)
 
+    # If there's an "error" field, return only those error fields. Otherwise, return IP context.
     if "error" in fetched_data.keys():
         return render_template('index.html', error=fetched_data["error"], reason=fetched_data["reason"])
 
@@ -57,9 +60,10 @@ def home():
 
 @app.route("/", methods=['POST'])
 def searchIP():
-    ip_address = request.form.get('ip_address')
-    fetched_data = ip_query(ip_address)
+    ip_address = request.form.get('ip_address') # Fetch the received form-data.
+    fetched_data = ip_query(ip_address) # And then query the given data to the API.
 
+    # If there's an "error" field, return only those error fields. Otherwise, return IP context.
     if "error" in fetched_data.keys():
         return render_template('index.html', error=fetched_data["error"], reason=fetched_data["reason"])
 
@@ -78,6 +82,6 @@ def searchIP():
                                ip_lng=fetched_data["lng"],
                                )
 
-
+# Driver Code
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
